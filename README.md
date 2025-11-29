cea790c817f3447debf064efe6869d599ce54bae
moxiivx <moxiix@proton.me>
2025-11-29 23:39:31 +0100

    add bash script and manifests
    Signed-off-by: moxiivx <moxiix@proton.me >

---
+++ README.md
@@ -0,0 +1,9 @@
+## Manifests
+my manifests for aosp
+
+to apply:
+```
+cd ~/manifests
+./init_a53.sh
+```
+> for now it's only for avium and im gonna prob add more roms in the future. so it might change
---
+++ init_a53.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+###-------------------------
+###         vars
+###-------------------------
+
+# xmls
+s5e8825xml="$HOME/manifests/a53/s5e8825.xml"
+mainmanifest="$HOME/manifests/a53/manifest.xml"
+
+# path to copy to
+pathtcpt1="$HOME/roms/avium/.repo/"
+pathtcpt2="$HOME/roms/avium/.repo/manifests/"
+
+###--------------------------
+###      copying stuff
+###--------------------------
+
+# copy stuff
+echo "copying mainfests"
+cp "$mainmanifest" "$pathtcpt1"
+cp "$s5e8825xml" "$pathtcpt2"
+echo "done..."
